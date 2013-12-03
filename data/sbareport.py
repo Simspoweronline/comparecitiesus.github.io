565a3bf7ff8450413917de27e845f2683ba5cae3
Paulo Da Silva <paulo@dasilva.pw>
2013-12-03 15:40:49 -0500

    Implemented loadcity

--- data/sbareport.py
+++ data/sbareport.py
@@ -1,9 +1,10 @@
+import cities
 import states
 import json
 import urllib2
 
 BASE_URL = "http://api.sba.gov/geodata/city_data_for_state_of/%s.json"
-SBA_PATH = "sba/%s.json"
+REPORT_PATH = "sba/%s.json"
 
 def downloadStateReport(state, filename):
     stateUrl = BASE_URL % state
@@ -11,8 +12,19 @@ def downloadStateReport(state, filename):
     stateFile = open(filename, "w")
     json.dump(report, stateFile)
 
+def findCity(city, report):
+    for currentCity in report:
+        if cities.cityNameEquals(city, currentCity["name"]):
+            return currentCity
+
+def loadCity(city, state):
+    try:
+        filename = REPORT_PATH % state
+        reports = json.load(open(filename))
+        return findCity(city, reports)
+    except IOError:
+        raise LookupError(city)
+        
 if __name__ == "__main__":
-    for state in states.getStates():
-        print state
-        filename = SBA_PATH % state
-        downloadStateReport(state, filename)
+    print loadCity("Miami", "FL")
+    print loadCity("Chicago", "IL")
